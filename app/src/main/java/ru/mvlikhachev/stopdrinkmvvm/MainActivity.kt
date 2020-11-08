031e26893fe3702fcb5fa8ddf20093d8fc99ef01
Flop88 <wwwqwwwqwwwqwwwq@gmail.com>
2020-11-08 11:17:46 +0300

    Object initialization in MainActivity

--- app/src/main/java/ru/mvlikhachev/stopdrinkmvvm/MainActivity.kt
+++ app/src/main/java/ru/mvlikhachev/stopdrinkmvvm/MainActivity.kt
@@ -2,10 +2,35 @@ package ru.mvlikhachev.stopdrinkmvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import ru.mvlikhachev.stopdrinkmvvm.databinding.ActivityMainBinding
+import ru.mvlikhachev.stopdrinkmvvm.utilits.APP_ACTIVITY
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mToolbar: Toolbar
+    lateinit var mNavController: NavController
+
+    private var _binding: ActivityMainBinding? = null
+    val mBinding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+        APP_ACTIVITY = this
+
+        mToolbar = mBinding.toolbar
+        mNavController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        setSupportActionBar(mToolbar)
+        setTitle(R.string.app_name)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
---
+++ app/src/main/java/ru/mvlikhachev/stopdrinkmvvm/utilits/constants.kt
@@ -0,0 +1,5 @@
+package ru.mvlikhachev.stopdrinkmvvm.utilits
+
+import ru.mvlikhachev.stopdrinkmvvm.MainActivity
+
+lateinit var APP_ACTIVITY: MainActivity
