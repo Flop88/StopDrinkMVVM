8ff76482d2c02c60dd36ec58bc9fdfe2c19c7783
Flop88 <wwwqwwwqwwwqwwwq@gmail.com>
2020-11-08 12:13:55 +0300

    Create ProfileFragmentViewModel, initializate, implement navigation profile-to-main

--- app/src/main/java/ru/mvlikhachev/stopdrinkmvvm/screens/profile/ProfileFragment.kt
+++ app/src/main/java/ru/mvlikhachev/stopdrinkmvvm/screens/profile/ProfileFragment.kt
@@ -1,4 +1,4 @@
-package ru.mvlikhachev.stopdrinkmvvm.screens.main
+package ru.mvlikhachev.stopdrinkmvvm.screens.profile
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -8,29 +8,32 @@ import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import ru.mvlikhachev.stopdrinkmvvm.R
 import ru.mvlikhachev.stopdrinkmvvm.databinding.FragmentMainBinding
+import ru.mvlikhachev.stopdrinkmvvm.databinding.FragmentProfileBinding
+import ru.mvlikhachev.stopdrinkmvvm.screens.main.MainFragmentViewModel
 import ru.mvlikhachev.stopdrinkmvvm.utilits.APP_ACTIVITY
 
-class MainFragment : Fragment() {
+class ProfileFragment : Fragment() {
 
-    private var _binding : FragmentMainBinding? = null
+    private var _binding : FragmentProfileBinding ? = null
     private val mBinding get() = _binding!!
 
-    private lateinit var mViewModel: MainFragmentViewModel
+    private lateinit var mViewModel: ProfileFragmentViewModel
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentMainBinding.inflate(layoutInflater, container,false)
+        _binding = FragmentProfileBinding.inflate(layoutInflater, container,false)
         return mBinding.root
     }
 
     private fun initialization() {
-        mViewModel = ViewModelProvider(this).get(MainFragmentViewModel::class.java)
+        mViewModel = ViewModelProvider(this).get(ProfileFragmentViewModel::class.java)
         mBinding.bottomNavigationView.setOnNavigationItemSelectedListener {item ->
             when(item.itemId) {
-                R.id.profile_page -> {
-                    APP_ACTIVITY.mNavController.navigate(R.id.action_mainFragment_to_profileFragment)
+                R.id.main_page -> {
+                    APP_ACTIVITY.mNavController.navigate(R.id.action_profileFragment_to_mainFragment)
                 }
             }
             return@setOnNavigationItemSelectedListener true
---
+++ app/src/main/java/ru/mvlikhachev/stopdrinkmvvm/screens/profile/ProfileFragmentViewModel.kt
@@ -0,0 +1,8 @@
+package ru.mvlikhachev.stopdrinkmvvm.screens.profile
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class ProfileFragmentViewModel(application: Application) : AndroidViewModel(application) {
+
+}
